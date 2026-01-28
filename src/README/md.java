024429a6adc31d2fa290642fb1ce00edf8270596
khushi <khushi@10.48.136.34>
2026-01-28 14:55:35 -0500

    Add feature work

--- src/README/md.java
+++ src/README/md.java
@@ -1,1 +1,8 @@
+package README;
+
+public class md {
+    public static void main(String[] args) {
+       System.out.println("ICE4 feature branch work");
+    }
+}
 
--- src/projectkhushi/Projectkhushi.java
+++ src/projectkhushi/Projectkhushi.java
@@ -10,11 +10,13 @@ package projectkhushi;
  */
 public class Projectkhushi {
     // ICE4 Git initial commit
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Working on feature branch");
     }
     
 }
